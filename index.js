91df30fde66cf318abcd9aa5d7d9026b601be05c
Scott Oster <artificialingenious@gmail.com>
2021-04-13 14:51:43 +0100

    add test function to index

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 function sayHello() {
-  return;
-  console.log('HI');
+  console.log('hi');
 }
 
 module.exports = { sayHello };
+
+const alphabetReplace = (str) => {
+  let indexString = '';
+  let charArray = str.split('');
+
+  charArray.forEach((element, i) => {
+    if (element.charCodeAt(0) > 90 && element.charCodeAt(0) !== 32) {
+      indexString += element.charCodeAt(0) - 96;
+    }
+    if (element.charCodeAt(0) < 90 && element.charCodeAt(0) !== 32) {
+      indexString += element.charCodeAt(0) - 64;
+    }
+    if (i < charArray.length - 1 && element.charCodeAt(0) !== 32) {
+      indexString += ' ';
+    }
+  });
+  return indexString;
+};
+
+module.exports = { alphabetReplace };
